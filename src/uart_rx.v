48ab7ff47d5b81b8661ccf40342b16d39e4ca03b
R0cinant3 <sindre_jf@hotmail.com>
2022-01-13 11:34:42 +0100

    Made the bare-bones structure of the repository

---
+++ src/uart_rx.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/13/2022 11:30:12 AM
+// Design Name: 
+// Module Name: uart_rx
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uart_rx(
+
+    );
+endmodule
